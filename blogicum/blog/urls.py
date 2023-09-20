955f38f9e23b21521f569a47cfd0f4d6eb88c68e
Дарья Галушко <dashuta.galushko@mail.ru>
2023-09-21 02:18:15 +0500

    текст коÐdone project

---
+++ blogicum/blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import category_posts, index, post_detail
+
+app_name = 'blog'
+
+urlpatterns = [
+    path('', index, name='index'),
+    path('category/<slug:category_slug>/', category_posts, name='category_posts'),
+    path('posts/<int:id>/', post_detail, name='post_detail')
+]
---
+++ blogicum/pages/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def about(request):
+    return render(request, template_name='pages/about.html')
+
+def rules(request):
+    return render(request, template_name='pages/rules.html')
